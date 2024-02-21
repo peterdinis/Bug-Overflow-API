0ea9536e77fdde10a52c5ed7d480f54946c927f5
Peter Dinis <peterdinis0@pobox.sk>
2024-02-21 19:26:09 +0100

    fix: Install missing package

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import errorHandler from 'errorhandler';
 import app from './app';
+import dotenv from "dotenv";
 
 if (process.env.NODE_ENV === 'development') {
     app.use(errorHandler());
 }
+
+dotenv.config();
 
 const PORT = process.env.PORT as unknown as number;
 
