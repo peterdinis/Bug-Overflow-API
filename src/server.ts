e73504256e8c650ae328125c5175c8806dd7da93
Peter Dinis <peterdinis0@pobox.sk>
2024-02-24 10:02:41 +0100

    feat: Create simple helper/check endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import { ApolloServer } from 'apollo-server';
 import { resolvers, typeDefs } from './graphql/appschema';
 import dotenv from 'dotenv';
+import express, { Application } from "express";
+
+const app: Application = express();
 
 const PORT = process.env.PORT as unknown as number;
 
@@ -13,6 +16,8 @@ const server = new ApolloServer({
 });
 
 dotenv.config();
+
+app.get("health", (_, res) => res.send("OK")); // Helper endpoint
 
 server.listen(PORT).then(({ url }) => {
     console.log('Applikácia beží na url ' + url);
