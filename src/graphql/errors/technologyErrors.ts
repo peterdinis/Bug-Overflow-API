1b803d08c62c36dc920c3c9238797fadfcff978f
Peter Dinis <peterdinis0@pobox.sk>
2024-02-28 18:49:31 +0100

    FOO

---
+++ src/graphql/errors/technologyErrors.ts
@@ -0,0 +1,9 @@
+import { GraphQLError } from 'graphql';
+
+// TODO: Toto neskôr pripraviť pre technology toto je len ukážka
+
+throw new GraphQLError('You are not authorized to perform this action.', {
+  extensions: {
+    code: 'FORBIDDEN',
+  },
+});
