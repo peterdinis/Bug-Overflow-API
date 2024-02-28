6ac0580ac77118f8ff7e9f04450d1827c9365f44
Peter Dinis <peterdinis0@pobox.sk>
2024-02-28 18:55:07 +0100

    feat: Updating appschema

--- src/graphql/appschema.ts
+++ src/graphql/appschema.ts
@@ -1,4 +1,3 @@
-import { gql } from 'apollo-server';
 import { technologyTypeDefs } from './technology/typeDefs';
 import { technologyResolvers } from './technology/resolvers';
 import { userTypeDefs } from './users/typeDefs';
@@ -6,20 +5,9 @@ import { userResolvers } from './users/resolvers';
 import { commentsTypeDefs } from './comments/typeDefs';
 import { postsTypeDefs } from './posts/typeDefs';
 import { makeExecutableSchema } from 'graphql-tools';
+import { rootResolvers } from './root/resolvers';
+import { rootTypeDefs } from './root/typedefs';
 
-const rootTypeDefs = gql`
-    type Query {
-        hello: String
-    }
-`;
-
-const rootResolvers = {
-    Query: {
-        hello: async () => {
-            return 'Hello';
-        },
-    },
-};
 
 const resolvers = [rootResolvers, technologyResolvers, userResolvers];
 const typeDefs = [rootTypeDefs, technologyTypeDefs, userTypeDefs, commentsTypeDefs, postsTypeDefs];
