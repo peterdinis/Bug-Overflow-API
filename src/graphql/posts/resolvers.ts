dda7cb1ae59e2c8764682c11cb713bc666855c65
Peter Dinis <peterdinis0@pobox.sk>
2024-03-01 20:19:11 +0100

    feat: New queries to posts resolvers

--- src/graphql/posts/resolvers.ts
+++ src/graphql/posts/resolvers.ts
@@ -1,1 +1,36 @@
-export const postsResolvers = {}
+import {prisma} from "../../prisma/db";
+import { ApolloError } from 'apollo-server';
+
+export const postsResolvers = {
+    Query: {
+        getAllPosts: async() => {
+            return prisma.post.findMany();
+        },
+
+        getPostById: async(id: number) => {
+            const findOnePost = await prisma.post.findUnique({
+                where: {
+                    id
+                }
+            });
+
+            if(!findOnePost) {
+                throw new ApolloError("Post with this id does not exists", "404");
+            }
+
+            return findOnePost;
+        },
+
+        paginatedPosts: async (
+            _: unknown,
+            { page = 1, pageSize = 10 },
+        ) => {
+            const posts = await prisma.post.findMany({
+                take: pageSize,
+                skip: (page - 1) * pageSize,
+            });
+
+            return posts;
+        },
+    }
+}
