d41710969f654a15614f624c25a7c5878b908595
Peter Dinis <peterdinis0@pobox.sk>
2024-03-03 15:58:37 +0100

    feat: Setup subscriptions

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,50 @@
 import { ApolloServer } from 'apollo-server';
+import express from "express";
 import dotenv from 'dotenv';
 import { resolvers, typeDefs } from './graphql/appschema';
+import { createServer } from 'http';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
+import { useServer } from 'graphql-ws/lib/use/ws';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
 
 const PORT = process.env.PORT as unknown as number;
 
+const app = express();
+
+const httpServer = createServer(app);
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/subscriptions',
+});
+
+const serverCleanup = useServer({ schema }, wsServer);
+
 const server = new ApolloServer({
-    resolvers,
-    typeDefs,
+    schema,
     cors: {
         origin: '*',
     },
     debug: true,
+    plugins: [
+  
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              console.log("PING");
+              await serverCleanup.dispose();
+            },
+          };
+        },
+      },
+    ],
 });
 
 dotenv.config();
