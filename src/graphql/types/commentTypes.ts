de98c5bac9a33430571f54831d9f10c451cfecdc
Peter Dinis <peterdinis0@pobox.sk>
2024-03-13 18:11:58 +0100

    feat: Finish basic comments resolvers

---
+++ src/graphql/types/commentTypes.ts
@@ -0,0 +1,25 @@
+export type CreateComment = {
+    commentedText: string;
+    postId: number;
+    userId: number;
+    upvote: number;
+    downvote: number;
+}
+
+export type UpdateComment = {
+    commentedText?: string;
+    postId?: number;
+    userId?: number;
+    upvote?: number;
+    downvote?: number;
+}
+
+export type UpvoteComment = {
+    commentId: number;
+    upvote: number;
+}
+
+export type DownvoteComment = {
+    commentId: number;
+    downvote: number;
+}
